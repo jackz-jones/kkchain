754f3d37851356dd15d5f84709e6ce8e4a17e927
kojhliang <459723919@qq.com>
2018-11-20 16:58:36 +0800

    MOD:cancel checknonce

--- core/state_transition.go
+++ core/state_transition.go
@@ -151,14 +151,14 @@ func (st *StateTransition) buyGas() error {
 
 func (st *StateTransition) preCheck() error {
 	// Make sure this transaction's nonce is correct.
-	if st.msg.CheckNonce() {
-		nonce := st.state.GetNonce(st.msg.From())
-		if nonce < st.msg.Nonce() {
-			return ErrNonceTooHigh
-		} else if nonce > st.msg.Nonce() {
-			return errNonceTooLow
-		}
-	}
+	// if st.msg.CheckNonce() {
+	// 	nonce := st.state.GetNonce(st.msg.From())
+	// 	if nonce < st.msg.Nonce() {
+	// 		return ErrNonceTooHigh
+	// 	} else if nonce > st.msg.Nonce() {
+	// 		return errNonceTooLow
+	// 	}
+	// }
 	return st.buyGas()
 }
 
